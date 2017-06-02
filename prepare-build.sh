cb5218ab345e926db3986cc4e492bd430144ae93
Florent Revest <revestflo@gmail.com>
2017-06-02 11:34:01 +0200

    Update OE and bitbake to pyro

--- prepare-build.sh
+++ prepare-build.sh
@@ -51,11 +51,11 @@ else
     fi
 
     # Fetch all the needed layers in src/
-    clone_dir src/oe-core              https://github.com/openembedded/openembedded-core.git morty
-    clone_dir src/oe-core/bitbake      https://github.com/openembedded/bitbake.git           1.32
-    clone_dir src/meta-openembedded    https://github.com/openembedded/meta-openembedded.git morty
+    clone_dir src/oe-core              https://github.com/openembedded/openembedded-core.git pyro
+    clone_dir src/oe-core/bitbake      https://github.com/openembedded/bitbake.git           1.34
+    clone_dir src/meta-openembedded    https://github.com/openembedded/meta-openembedded.git pyro
     clone_dir src/meta-qt5             https://github.com/meta-qt5/meta-qt5.git              master
-    clone_dir src/meta-smartphone      https://github.com/shr-distribution/meta-smartphone   morty
+    clone_dir src/meta-smartphone      https://github.com/shr-distribution/meta-smartphone   pyro
     clone_dir src/meta-asteroid        https://github.com/AsteroidOS/meta-asteroid           master
     clone_dir src/meta-anthias-hybris  https://github.com/AsteroidOS/meta-anthias-hybris     master
     clone_dir src/meta-bass-hybris     https://github.com/AsteroidOS/meta-bass-hybris        master
