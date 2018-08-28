9b84709e6a0e9bc19814ddca7ae5704cbfd7d4b1
Florent Revest <revestflo@gmail.com>
2018-08-28 19:56:41 +0200

    Upgrade OpenEmbedded to Sumo

--- prepare-build.sh
+++ prepare-build.sh
@@ -66,11 +66,11 @@ else
     fi
 
     # Fetch all the needed layers in src/
-    clone_dir src/oe-core              https://github.com/openembedded/openembedded-core.git rocko
-    clone_dir src/oe-core/bitbake      https://github.com/openembedded/bitbake.git           1.36
-    clone_dir src/meta-openembedded    https://github.com/openembedded/meta-openembedded.git rocko
+    clone_dir src/oe-core              https://github.com/openembedded/openembedded-core.git sumo
+    clone_dir src/oe-core/bitbake      https://github.com/openembedded/bitbake.git           1.38
+    clone_dir src/meta-openembedded    https://github.com/openembedded/meta-openembedded.git sumo
     clone_dir src/meta-qt5             https://code.qt.io/yocto/meta-qt5.git                 5.10   v5.10.0
-    clone_dir src/meta-smartphone      https://github.com/shr-distribution/meta-smartphone   rocko
+    clone_dir src/meta-smartphone      https://github.com/shr-distribution/meta-smartphone   sumo
     clone_dir src/meta-asteroid        https://github.com/AsteroidOS/meta-asteroid           master
     clone_dir src/meta-anthias-hybris  https://github.com/AsteroidOS/meta-anthias-hybris     master
     clone_dir src/meta-bass-hybris     https://github.com/AsteroidOS/meta-bass-hybris        master
