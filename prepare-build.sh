aed65d5d67edf9419eb2abc9bd9216bbb3143816
jrt <git@jrtberlin.de>
2021-10-15 01:46:24 +0200

    add qemux86 build target (#170)

--- prepare-build.sh
+++ prepare-build.sh
@@ -14,7 +14,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-declare -a devices=("anthias" "bass" "dory" "harmony" "inharmony" "lenok" "mooneye" "smelt" "sparrow" "sprat" "sturgeon" "sawfish" "swift" "tetra" "wren")
+declare -a devices=("anthias" "bass" "dory" "harmony" "inharmony" "lenok" "mooneye" "qemux86" "smelt" "sparrow" "sprat" "sturgeon" "sawfish" "swift" "tetra" "wren")
 
 function printNoDeviceInfo {
     echo "Usage:"
